f69a5d7be398c7cc1db86eff16ddc6e7b1164cad
Julian Haag <me@jhaag.me>
2022-02-27 13:09:00 +0100

    Publish

---
+++ index.js
@@ -0,0 +1,134 @@
+var Typer = {
+  text: '',
+  accessCountimer: null,
+  index: 0,
+  speed: 2,
+  file: '',
+  accessCount: 0,
+  deniedCount: 0,
+  init: function () {
+    accessCountimer = setInterval(function () {
+      Typer.updLstChr();
+    }, 500);
+    $.get(Typer.file, function (data) {
+      Typer.text = data;
+      Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+    });
+  },
+
+  content: function () {
+    return $('#console').html();
+  },
+
+  write: function (str) {
+    $('#console').append(str);
+    return false;
+  },
+
+  addText: function (key) {
+    if (key.keyCode == 18) {
+      Typer.accessCount++;
+
+      if (Typer.accessCount >= 3) {
+        Typer.makeAccess();
+      }
+    } else if (key.keyCode == 20) {
+      Typer.deniedCount++;
+
+      if (Typer.deniedCount >= 3) {
+        Typer.makeDenied();
+      }
+    } else if (key.keyCode == 27) {
+      Typer.hidepop();
+    } else if (Typer.text) {
+      var cont = Typer.content();
+      if (cont.substring(cont.length - 1, cont.length) == '|')
+        $('#console').html(
+          $('#console')
+            .html()
+            .substring(0, cont.length - 1),
+        );
+      if (key.keyCode != 8) {
+        Typer.index += Typer.speed;
+      } else {
+        if (Typer.index > 0) Typer.index -= Typer.speed;
+      }
+      var text = Typer.text.substring(0, Typer.index);
+      var rtn = new RegExp('\n', 'g');
+
+      $('#console').html(text.replace(rtn, '<br/>'));
+      window.scrollBy(0, 50);
+    }
+
+    if (key.preventDefault && key.keyCode != 122) {
+      key.preventDefault();
+    }
+
+    if (key.keyCode != 122) {
+      // otherway prevent keys default behavior
+      key.returnValue = false;
+    }
+  },
+
+  updLstChr: function () {
+    var cont = this.content();
+
+    if (cont.substring(cont.length - 1, cont.length) == '|')
+      $('#console').html(
+        $('#console')
+          .html()
+          .substring(0, cont.length - 1),
+      );
+    else this.write('|'); // else write it
+  },
+};
+
+function replaceUrls(text) {
+  var http = text.indexOf('http://');
+  var space = text.indexOf('.me ', http);
+
+  if (space != -1) {
+    var url = text.slice(http, space - 1);
+    return text.replace(url, '<a href="' + url + '">' + url + '</a>');
+  } else {
+    return text;
+  }
+}
+
+Typer.speed = 3;
+Typer.file = 'newspicel.txt';
+Typer.init();
+
+var time = 30;
+
+var defaultTimer;
+var timer;
+
+function type() {
+  if (Typer.index > Typer.text.length) {
+    clearInterval(defaultTimer);
+  }else{
+    Typer.addText({ keyCode: 123748 });
+  }
+}
+
+function increaseSpeed() {
+  clearInterval(defaultTimer);
+  clearInterval(timer);
+
+  if (Typer.index < Typer.text.length) {
+    timer = setInterval('type();', 0);
+  }
+}
+
+window.onload = function () {
+  defaultTimer = setInterval('type();', time);
+  console.log('Click to anywhere increase Speed')
+};
+
+window.onclick = function () {
+  clearInterval(defaultTimer);
+  for (var i = 0; i < Typer.text.length + 5; i++) {
+    type();
+  }
+};
